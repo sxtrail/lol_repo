e17fa47f9f56d6b6b4fdeb66d6fcee0fdab954d4
sxtrail <matthew.bezak@yahoo.com>
2018-10-04 20:10:28 +0200

    First commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
